b876ad6f4419e71f0ed5b238529cdac78663d6e0
jaitken <jaitken97@gmail.com>
2021-04-23 13:17:38 -0400

    added photo gallery component

--- src/components/PhotoGallery/PhotoGallery.vue
+++ src/components/PhotoGallery/PhotoGallery.vue
@@ -1,8 +1,20 @@
 <template>
   <div id="gallery">
       <div v-if="bDataLoaded">
-        <div id="container1">
+        <div id="c1" class="bigContainer">
           <img v-bind:src="`${photos[0].url}`" alt="photo" width="100%" height="100%">
+        </div>
+        <div id="container2" class="littleContainer">
+          <img v-bind:src="`${photos[1].url}`" alt="photo" width="100%" height="100%">
+        </div>
+        <div id="container3" class="littleContainer">
+          <img v-bind:src="`${photos[2].url}`" alt="photo" width="100%" height="100%">
+        </div>
+        <div id="container2" class="littleContainer">
+          <img v-bind:src="`${photos[3].url}`" alt="photo" width="100%" height="100%">
+        </div>
+        <div id="container4" class="littleContainer">
+          <img v-bind:src="`${photos[4].url}`" alt="photo" width="100%" height="100%">
         </div>
       </div>
   </div>
