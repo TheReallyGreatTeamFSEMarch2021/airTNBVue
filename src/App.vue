10b85550ac59deb92ed28bb9d0d4a842b150625c
Fei Yang <fyang@C02CMKHKML7H.fios-router.home>
2021-04-26 13:58:41 -0400

    wireframe and basic routing

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">    
+     
+    <div class="row col-12">
+      <h1>NavBar Vue Component</h1>
+    </div>
+    <router-view></router-view>  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+//can pass in props through the component
+//they have lifecycle methods -- can make axios calls through the lifecycle methods
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
-</script>
+</script >
 
-<style>
+<style >
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,4 +30,53 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+#app * {
+  border: 1px solid green;
+  box-sizing: border-box;
+  margin:0px;
+  padding:0px;
+}
+
+h1{
+    color:darkblue;
+  }
+  
+  h2{
+    color:darkgreen;
+  }
+
+  body{
+    margin:0px;
+    padding:0px;
+  }
+  
+  /* added by Fei */
+  .row::after{
+    content: "";
+    clear: both;
+    display: table;
+  }
+  
+  [class*="col-"]{
+    float:left;
+    padding:2px;
+  }
+  
+  .col-1 {width: 8.33%;}
+  .col-2 {width: 16.66%;}
+  .col-3 {width: 25%;}
+  .col-4 {width: 33.33%;}
+  .col-5 {width: 41.66%;}
+  .col-6 {width: 50%;}
+  .col-7 {width: 58.33%;}
+  .col-8 {width: 66.66%;}
+  .col-9 {width: 75%;}
+  .col-10 {width: 83.33%;}
+  .col-11 {width: 91.66%;}
+  .col-12 {width: 100%;}
+  
+
+
+  
 </style>
---
+++ src/components/ListingIndex/ListingIndex.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="listingIndex">
+     <h1>ListingIndex</h1>
+  </div>
+</template>
+
+<style scoped src="./ListingIndex.css">
+</style>
+<script>
+  import axios from 'axios';
+  export default {
+    name: 'ListingIndex',
+    
+    props: {
+      
+    },
+    data(){
+        return{
+          
+        }
+    },
+    mounted(){
+          
+    }
+    
+  }
+</script>
