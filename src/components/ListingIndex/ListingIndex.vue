e80b0cb34a75aa4f9871a35fb73939423664cdf3
Eliotdoesprogramming <zubdoob@gmail.com>
2021-04-27 14:54:27 -0500

    static map component

--- src/components/ListingIndex/ListingIndex.vue
+++ src/components/ListingIndex/ListingIndex.vue
@@ -1,13 +1,24 @@
 <template>
   <div id="listingIndex">
      <h1>ListingIndex</h1>
+     <p>hi</p>
+    <div style="height:1000px;width:1000px;">
+        <iframe
+        width="600"
+        height="450"
+        style="border:0"
+        loading="lazy"
+        allowfullscreen
+        src="`https://www.google.com/maps/embed/v1/place?key=AIzaSyAlQlrrSaPgoOxEwXgui45oBC97YQC1lfs&q=Space+Needle,Seattle+WA`">
+    </iframe>
+    </div>
   </div>
+
 </template>
 
 <style scoped src="./ListingIndex.css">
 </style>
 <script>
-  import axios from 'axios';
   export default {
     name: 'ListingIndex',
     
