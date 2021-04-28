73b00e326a416c40a7b597eaffb3e9e6106047d2
Nathaniel Eveland <54957992+neveland98@users.noreply.github.com>
2021-04-28 16:58:04 -0500

    added house rules to thingstoknow component

--- src/components/ThingsToKnow/ThingsToKnow.vue
+++ src/components/ThingsToKnow/ThingsToKnow.vue
@@ -1,13 +1,23 @@
 <template>
   <div id="thingsToKnow">
-     <h1>Things To Know</h1>
+     <h3>Things To Know</h3>
+     <div class="row col-4">
+       <h4>House Rules</h4>
+        <p>Check in: {{this.listing.ruleList.checkInTime}}</p>
+        <p>Check out: {{this.listing.ruleList.checkOutTime}}</p>
+        <p v-if='regex.test(this.listing.ruleList.checkInMethod)'>Self check-in with {{this.listing.ruleList.checkInMethod.split('_')[1].toLowerCase()}}</p>
+        <p v-if="!listing.ruleList.smokingAllowed">No smoking</p>
+        <p v-if="!listing.ruleList.petsAllowed">No pets</p>
+        <p v-if="!listing.ruleList.childSuitable">Not suitable for children</p>
+        <p v-if="!listing.ruleList.partiesAllowed">No parties or events</p>
+     </div><div class="row col-4">Health &amp; safety</div><div class="row col-4">Cancellation policy</div>
   </div>
 </template>
 
 <style scoped src="./ThingsToKnow.css">
 </style>
 <script>
-//   import axios from 'axios';
+  import axios from 'axios';
   export default {
     name: 'ThingsToKnow',
     
@@ -16,11 +26,14 @@
     },
     data(){
         return{
+          listing: null,
+          regex: new RegExp("SELF_*")
           
         }
     },
     mounted(){
-          
+          axios.get('http://localhost:8080/api/listing/getById/1')
+          .then(response=>this.listing=response.data);
     }
     
   }
