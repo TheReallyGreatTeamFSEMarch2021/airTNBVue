5c8c383440b34313246ccc430c49dfe2654341f0
Roberto Sanchez <rsanchez92@live.com>
2021-04-28 15:19:47 -0700

    Completed basic structure with styles.

--- src/components/DescriptionBox/Description.vue
+++ src/components/DescriptionBox/Description.vue
@@ -1,18 +1,115 @@
 <template>
   <div id="descriptionPanel">
-      <h3>PLACE NAME</h3>
-      <span>Number of Guests</span><span>Type of Place</span><span>Number of Rooms</span><span>number of bathrooms</span> <span>HostImage</span>
+      <h3>{{data.typeOfPlace}} hosted by [HostName]</h3>
+      <span> [0] guests</span><span>[type of place]</span><span>[0] beds</span><span>[0] bath</span> <span>[HostImage]</span>
       <hr>
-        <div>
-            <p>Type of Place: {{data.typeOfPlace}}</p>
-            <p>Cancellation Date: {{data.cancellationDate}}</p>
+        <div id="detailsSection">
+            <div class="grid-container">
+                <div class="">
+                    <span class="detailIcon">🏠</span>
+                </div>
+                <div class="subDetail grid-child">
+                    <span class="detailHeading">
+                        Entire {{data.typeOfPlace}}
+                    </span>
+                    <br>
+                    <span class="detailSubHeading">
+                        You'll have the {{data.typeOfPlace}} to yourself.
+                    </span>
+                </div>
+            </div>
+            <div class="grid-container">
+                <div class="">
+                    <span class="detailIcon">📅</span>
+                </div>
+                <div class="grid-child">
+                    <span class="detailHeading">
+                    Free cancellation until {{data.cancellationDate}}
+                    </span>
+                    <br>
+                    <span class="detailSubHeading">
+                        After that, cancel before [HOUR AM/PM] on [DATE] and get a full refund, minus the first night and service fee.
+                    </span>
+                </div>
+                
+            </div>
+            <div class="grid-container">
+                <div class="">
+                    <span class="detailIcon">⭐</span>
+                </div>
+                <div class="grid-child">
+                    <span class="detailHeading">
+                    Enchanced Clean
+                    </span>
+                    <br>
+                    <span class="detailSubHeading">
+                        This host commited to AirTnb  5 - step enhanced cleaning process. Learn more.
+                    </span>
+                </div>
+                
+            </div>
+            <div class="grid-container">
+                <div class="">
+                    <span class="detailIcon">📋</span>
+                </div>
+                <div class="grid-child">
+                    <span class="detailHeading">
+                    House rules
+                    </span>
+                    <br>
+                    <span class="detailSubHeading">
+                        The host does allow pets, parties, and smoking.
+                    </span>
+                </div>
+                
+            </div>
         </div>
       <hr>
-      <p>Description: {{data.smallDescription}}</p>
+      <div id="descriptionBlurb">
+            {{data.smallDescription}}
+      </div>
+      <p></p>
   </div>
 </template>
 
 <style scoped>
+.grid-container {
+    display: grid;
+    grid-template-columns: 100px auto;
+}
+.detailHeading{
+
+    
+    font-weight: 400 !important;
+    font-size: 16px !important;
+    line-height: 20px !important;
+}
+.detailSubHeading {
+    font-size: 14px;
+    color: rgb(113, 113, 113);
+    font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", sans-serif !important;
+}
+.grid-child{
+    text-align: left;
+}
+#descriptionBlurb{
+    text-align: left;
+    margin-top: 10px;
+    font-size: 12px;
+
+}
+#descriptionPanel{
+    width:500px;
+}
+#reviewNumber{
+
+}
+#placeDetails{
+
+}
+#hostImage{
+
+}
 </style>
 <script>
   import axios from 'axios';
@@ -31,14 +128,14 @@
         // Get id from url to get description
           axios({
               method: 'GET',
-              url: 'http://localhost:8080/api/description/7',
+              url: 'http://localhost:8080/api/description/24',
               headers: '',
           })
           .then(response=>{
               console.table(response.data)
               this.data = response.data
           })
-          .catch((error)=>{
+          .catch((error) => {
               switch(error.response.status){
                   case 400:
                       console.log('An error has occurred.')
