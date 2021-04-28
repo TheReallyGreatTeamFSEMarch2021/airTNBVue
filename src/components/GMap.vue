daac0d34e7f621efdbc4d260c1a3642cb5e248aa
Eliotdoesprogramming <zubdoob@gmail.com>
2021-04-28 12:39:17 -0500

    removing api key from public internet

--- src/components/GMap.vue
+++ src/components/GMap.vue
@@ -1,19 +1,19 @@
 <template>
 <div>
-    
     <iframe width="600" 
       height="450" 
       style="border:0" 
       loading="lazy" 
       allowfullscreen 
         :src="`https://www.google.com/maps/embed/v1/place?`+
-        `key=AIzaSyAlQlrrSaPgoOxEwXgui45oBC97YQC1lfs&`+
+        `key=${getApiKey()}&`+
         `q=${lat}%20-${long}`">
     </iframe>
 </div>
 </template>
 
 <script>
+import token from "../assets/mapskey.json"
 export default {
     name:"GMap",
     props:{
@@ -21,6 +21,12 @@ export default {
         // following Fei's structure from database
         lat:Number,
         long:Number
+    },
+    methods:{
+        getApiKey(){
+            console.log(token);
+            return token.token;
+        }
     }
 }
 </script>
--- src/components/ListingIndex/ListingIndex.vue
+++ src/components/ListingIndex/ListingIndex.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="listingIndex">
      <h1>ListingIndex</h1>
+     <GMap
+     :lat="42.72"
+     :long="78.01"
+     />
   </div>
 
 </template>
 
 <script>
+import GMap from '../GMap.vue'
   export default {
     name: 'ListingIndex',
     props: {
     },
     components:{
+        GMap
     }
     ,
     data(){
