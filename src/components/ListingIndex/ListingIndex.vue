d16cfa6d8287334f7773cc1c22c9d9971157f9aa
jaitken <jaitken97@gmail.com>
2021-04-28 15:40:32 -0400

    removed photoGallery from listingIndex

--- src/components/ListingIndex/ListingIndex.vue
+++ src/components/ListingIndex/ListingIndex.vue
@@ -5,21 +5,18 @@
      :lat="42.72"
      :long="78.01"
      />
-     <PhotoGallery/>
   </div>
 
 </template>
 
 <script>
 import GMap from '../GMap.vue'
-import PhotoGallery from '../PhotoGallery/PhotoGallery.vue'
   export default {
     name: 'ListingIndex',
     props: {
     },
     components:{
         GMap,
-        PhotoGallery
     }
     ,
     data(){
