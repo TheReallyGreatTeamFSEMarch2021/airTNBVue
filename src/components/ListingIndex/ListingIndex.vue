bf2bdad4993f59a62e73f0f7e09a50d280858b9e
Eliotdoesprogramming <zubdoob@gmail.com>
2021-04-29 09:18:02 -0500

    cleanup testing code

--- src/components/ListingIndex/ListingIndex.vue
+++ src/components/ListingIndex/ListingIndex.vue
@@ -1,22 +1,17 @@
 <template>
   <div id="listingIndex">
      <h1>ListingIndex</h1>
-     <GMap
-     :lat="42.72"
-     :long="78.01"
-     />
   </div>
 
 </template>
 
 <script>
-import GMap from '../GMap.vue'
   export default {
     name: 'ListingIndex',
     props: {
     },
     components:{
-        GMap,
+
     }
     ,
     data(){
