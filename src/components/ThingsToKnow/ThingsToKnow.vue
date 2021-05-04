0564988da934493e5a428bbc78f79c69ee45ef61
neveland98 <54957992+neveland98@users.noreply.github.com>
2021-05-04 15:49:18 -0500

    added more to health and safety info section

--- src/components/ThingsToKnow/ThingsToKnow.vue
+++ src/components/ThingsToKnow/ThingsToKnow.vue
@@ -6,17 +6,24 @@
         <p>Check in: {{listing.ruleList.checkInTime}}</p>
         <p>Check out: {{listing.ruleList.checkOutTime}}</p>
         <p v-if='regex.test(listing.ruleList.checkInMethod)&&listing.ruleList.checkInMethod!="SELF_SMART_LOCK"'>Self check-in with {{listing.ruleList.checkInMethod.split('_')[1].toLowerCase()}}</p>
+        <p v-else-if='regex.test(listing.ruleList.checkInMethod)'>Self check-in with smart lock</p>
         <p v-if="!listing.ruleList.smokingAllowed">No smoking</p>
+        <p v-else>Smoking allowed</p>
         <p v-if="!listing.ruleList.petsAllowed">No pets</p>
-        <p v-if="!listing.ruleList.childSuitable">Not suitable for children</p>
+        <p v-else>Pets allowed</p>
+        <p v-if="!listing.ruleList.childSuitable">Not suitable for children and infants</p>
         <p v-if="!listing.ruleList.partiesAllowed">No parties or events</p>
+        <p v-else>Parties allowed</p>
     </div>
     <div class="row col-4">
         <h4>Health &amp; safety</h4>
         <p v-if="listing.healthAndSafetyInfo.enhancedCleaning">Committed to AirTNP's enhanced cleaning process</p>
         <p>AirTNP's social-distancing and other COVID-19-related guidelines apply</p>
         <p v-if="listing.healthAndSafetyInfo.cameraInfo!=null">Security camera/recording device</p>
-        <!-- todo: this doesn't work -->
+        <p v-if='listing.ruleList.smokeAlarm'>Smoke alarm</p>
+        <p v-else>No smoke alarm reported</p>
+        <p v-if='listing.ruleList.cMonAlarm'>Carbon monoxide alarm</p>
+        <p v-else>No carbon monoxide alarm reported</p>
 
     </div>
     <div class="row col-4">
