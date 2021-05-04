0d279a2e4565aa0267f2e5b58e5c73b5eab30f52
jaitken <jaitken97@gmail.com>
2021-05-04 17:00:36 -0400

    added fullGallery div and button to show and hide

--- src/components/PhotoGallery/PhotoGallery.vue
+++ src/components/PhotoGallery/PhotoGallery.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="this.photos && this.photos.length > 4" id="gallery">
+  <div v-if="this.photos && this.photos.length > 4" id="initalDisplay">
         <div id="c1" class="bigContainer">
           <img v-bind:src="`${photos[0].url}`" alt="photo" width="100%" height="100%">
         </div>
@@ -14,9 +14,23 @@
         </div>
         <div id="c5" class="littleContainer">
           <img v-bind:src="`${photos[4].url}`" alt="photo" width="100%" height="100%">
-          <button class='btn'>Show All Photos</button>
+          <button class='showbtn' @click='toggle = !toggle'>Show All Photos</button>
         </div>
+        <div v-show='toggle' id='fullGallery'>
+          <button class='hidebtn' @click='toggle = !toggle'>X</button>
+          <div id='gallery'>
+            <div id="prev" @click="prevPhoto"> &lt; </div>
+            <div id="photoDisplay">
+              <div id='photoContainer'>
+                <img v-bind:src="`${displayUrl}`">
+              </div> 
+            </div>
+            <div id="next" @click="nextPhoto"> &gt; </div>
+          </div>
+        </div>
+
   </div>
+  
 </template>
 
 <style scoped src="./GalleryStyle.css">
@@ -35,9 +49,22 @@ export default {
 
   data(){
       return{
-          bDataLoaded: true
+          bDataLoaded: true,
+          toggle: false,
+          displayUrl: "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
       }
   },
+
+  methods:{
+    prevPhoto: function(){
+      console.log("prev");
+    },
+
+    nextPhoto: function(){
+      console.log('next');
+    }
+
+  }
 
 }
 </script>
