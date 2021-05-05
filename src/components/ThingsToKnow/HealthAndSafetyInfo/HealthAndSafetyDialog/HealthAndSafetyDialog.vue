991bf52fb94081914d300d15649050461b38166f
neveland98 <54957992+neveland98@users.noreply.github.com>
2021-05-05 17:02:02 -0500

    fixed dialog, added info to it, also added an icon

--- src/components/ThingsToKnow/HealthAndSafetyInfo/HealthAndSafetyDialog/HealthAndSafetyDialog.vue
+++ src/components/ThingsToKnow/HealthAndSafetyInfo/HealthAndSafetyDialog/HealthAndSafetyDialog.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-dialog v-if="healthAndSafetyInfo" max-width="600px">
+    <v-dialog transition="dialog-bottom-transition" v-if="healthAndSafetyInfo" max-width="600px">
         <template v-slot:activator="{ on }">
             <v-btn v-on="on">Show all</v-btn>
         </template>
@@ -7,8 +7,15 @@
             <v-card-title>
                 <h4>Health &amp; safety</h4>
             </v-card-title>
+            <v-divider/>
             <v-card-text>
-                
+              <p v-if="healthAndSafetyInfo.enhancedCleaning">Committed to AirTNP's enhanced cleaning process</p>
+              <p>AirTNP's social-distancing and other COVID-19-related guidelines apply</p>
+              <p v-if="healthAndSafetyInfo.cameraInfo!=null"><v-icon color=#000000 small>mdi-alert-circle</v-icon> Security camera/recording device</p>
+              <p v-if='healthAndSafetyInfo.smokeAlarm'>Smoke alarm</p>
+              <p v-else>No smoke alarm reported</p>
+              <p v-if='healthAndSafetyInfo.cMonAlarm'>Carbon monoxide alarm</p>
+              <p v-else>No carbon monoxide alarm reported</p>
             </v-card-text>
         </v-card>
     </v-dialog>
--- src/components/ThingsToKnow/HealthAndSafetyInfo/HealthAndSafetyInfo.vue
+++ src/components/ThingsToKnow/HealthAndSafetyInfo/HealthAndSafetyInfo.vue
@@ -3,11 +3,12 @@
         <h4>Health &amp; safety</h4>
         <p v-if="healthAndSafetyInfo.enhancedCleaning">Committed to AirTNP's enhanced cleaning process</p>
         <p>AirTNP's social-distancing and other COVID-19-related guidelines apply</p>
-        <p v-if="healthAndSafetyInfo.cameraInfo!=null">Security camera/recording device</p>
+        <p v-if="healthAndSafetyInfo.cameraInfo!=null"><v-icon color=#000000 small>mdi-alert-circle</v-icon> Security camera/recording device</p>
         <p v-if='healthAndSafetyInfo.smokeAlarm'>Smoke alarm</p>
         <p v-else>No smoke alarm reported</p>
         <p v-if='healthAndSafetyInfo.cMonAlarm'>Carbon monoxide alarm</p>
         <p v-else>No carbon monoxide alarm reported</p>
+        <br>
         <HealthAndSafetyDialog v-if="healthAndSafetyInfo" v-bind:healthAndSafetyInfo="healthAndSafetyInfo"/>
     </div>
 </template>
