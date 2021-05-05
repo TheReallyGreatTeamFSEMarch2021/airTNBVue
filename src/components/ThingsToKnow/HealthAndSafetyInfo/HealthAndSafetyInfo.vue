721803a0e11344b2f809721ec2abe395cc2a5c7b
Nathaniel Eveland <54957992+neveland98@users.noreply.github.com>
2021-05-05 13:41:51 -0500

    moved houserules and healthandsafetyinfo to separate sub-components for better readability

---
+++ src/components/ThingsToKnow/HealthAndSafetyInfo/HealthAndSafetyInfo.vue
@@ -0,0 +1,28 @@
+<template>
+    <div v-if="listing" class="row col-4">
+        <h4>Health &amp; safety</h4>
+        <p v-if="listing.healthAndSafetyInfo.enhancedCleaning">Committed to AirTNP's enhanced cleaning process</p>
+        <p>AirTNP's social-distancing and other COVID-19-related guidelines apply</p>
+        <p v-if="listing.healthAndSafetyInfo.cameraInfo!=null">Security camera/recording device</p>
+        <p v-if='listing.healthAndSafetyInfo.smokeAlarm'>Smoke alarm</p>
+        <p v-else>No smoke alarm reported</p>
+        <p v-if='listing.healthAndSafetyInfo.cMonAlarm'>Carbon monoxide alarm</p>
+        <p v-else>No carbon monoxide alarm reported</p>
+    </div>
+</template>
+
+<style scoped src="./HealthAndSafetyInfo.css">
+</style>
+<script>
+  export default {
+    name: 'HealthAndSafetyInfo',
+    
+    props: {
+      listing: Object
+    },
+    mounted(){
+          
+    }
+    
+  }
+</script>
---
+++ src/components/ThingsToKnow/HouseRules/HouseRules.vue
@@ -0,0 +1,38 @@
+<template>
+    <div v-if="listing" class="row col-4">
+       <h4>House Rules</h4>
+        <p>Check in: {{listing.ruleList.checkInTime}}</p>
+        <p>Check out: {{listing.ruleList.checkOutTime}}</p>
+        <p v-if='regex.test(listing.ruleList.checkInMethod)&&listing.ruleList.checkInMethod!="SELF_SMART_LOCK"'>Self check-in with {{listing.ruleList.checkInMethod.split('_')[1].toLowerCase()}}</p>
+        <p v-else-if='regex.test(listing.ruleList.checkInMethod)'>Self check-in with smart lock</p>
+        <p v-if="!listing.ruleList.smokingAllowed">No smoking</p>
+        <p v-else>Smoking allowed</p>
+        <p v-if="!listing.ruleList.petsAllowed">No pets</p>
+        <p v-else>Pets allowed</p>
+        <p v-if="!listing.ruleList.childSuitable">Not suitable for children and infants</p>
+        <p v-if="!listing.ruleList.partiesAllowed">No parties or events</p>
+        <p v-else>Parties allowed</p>
+    </div>
+</template>
+
+<style scoped src="./HouseRules.css">
+</style>
+<script>
+  export default {
+    name: 'HouseRules',
+    
+    props: {
+      listing: Object
+    },
+    data(){
+        return{
+          regex: new RegExp("^SELF_.*$")
+          
+        }
+    },
+    mounted(){
+          
+    }
+    
+  }
+</script>
