1db9b74c340c5dd3659f7d36459e56491c432b7b
jaitken <31525687+jaitken@users.noreply.github.com>
2021-05-07 16:28:58 -0400

    using env variable for api key (#26)

--- src/components/GMap.vue
+++ src/components/GMap.vue
@@ -14,7 +14,6 @@
 </template>
 
 <script>
-import token from "../assets/mapskey.json"
 export default {
     name:"GMap",
     props:{
@@ -25,7 +24,7 @@ export default {
     },
     methods:{
         getApiKey(){
-            return token.token;
+            return process.env.VUE_APP_MAPS_KEY;
         }
     }
 }
