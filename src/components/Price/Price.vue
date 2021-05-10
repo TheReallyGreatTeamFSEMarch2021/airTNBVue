17d997867785305dad89045870524f559fd877cd
Roberto Sanchez <rsanchez92@live.com>
2021-05-10 09:45:43 -0700

    Redesigned price component box

--- src/components/Price/Price.vue
+++ src/components/Price/Price.vue
@@ -1,58 +1,52 @@
 <template>
-  <v-card v-if="listing" id="priceBox" elevation="2" class="mx-auto" max-width="344" outlined>
+  <v-card
+    v-if="listing"
+    id="priceBox"
+    elevation="2"
+    class="mx-auto"
+    max-width="344"
+    outlined
+  >
     <div>
       <div id="title">
         <div class="inner">
-          <span id="pricePerNight"><span id="boldedPrice">${{ this.listing.price }} </span> / night</span>
+          <span id="pricePerNight"
+            ><span id="boldedPrice">${{ this.listing.price }} </span> /
+            night</span
+          >
         </div>
         <div class="inner">
-          <span id = "star-five"></span>
+          <span id="star-five"></span>
           <i class="fas fa-star fa-sm"></i>
-          <span id="averageReview">{{this.averageReview}} </span> 
-          </div>
+          <span id="averageReview">{{ this.averageReview }} </span>
+        </div>
       </div>
-      
       <br />
       <div id="calendar">[Calendar Goes here]</div>
       <v-btn elevation="2" id="reserveBtn" outlined text>Reserve</v-btn>
       <p>You won't be charged yet</p>
-      <table>
-        <tr>
-          <td class="charges">
-            ${{ this.listing.price }} x {{ this.daysSelected }} nights
-          </td>
-          <td class="chargePrice">
-            ${{ this.listing.price * this.daysSelected }}
-          </td>
-        </tr>
-        <tr>
-          <td class="charges">Cleaning Fee</td>
-          <td class="chargePrice">${{ this.cleaningFee }}</td>
-        </tr>
-        <tr>
-          <td class="charges">Service Fee</td>
-          <td class="chargePrice">${{ this.serviceFee }}</td>
-        </tr>
-        <tr>
-          <td class="charges">Occupancy Taxes and Fees</td>
-          <td class="chargePrice">${{ this.occupancyTaxesAndFees }}</td>
-        </tr>
-        <tr>
-          <td><hr /></td>
-          <td><hr /></td>
-        </tr>
-        <tr id="grandTotal">
-          <td class="charges">Total</td>
-          <td class="chargePrice">
-            ${{
-              this.listing.price * this.daysSelected +
-              this.cleaningFee +
-              this.serviceFee +
-              this.occupancyTaxesAndFees
-            }}
-          </td>
-        </tr>
-      </table>
+      <div id="tableRowGrid">
+        <div class="priceLabel">
+          ${{ this.listing.price }} x {{ this.daysSelected }} nights
+        </div>
+        <div class="price">${{ this.listing.price * this.daysSelected }}</div>
+        <div class="priceLabel">Cleaning Fee</div>
+        <div class="price">${{ this.cleaningFee }}</div>
+        <div class="priceLabel">Service Fee</div>
+        <div class="price">${{ this.serviceFee }}</div>
+        <div class="priceLabel">Occupancy Taxes and Fees</div>
+        <div class="price">${{ this.occupancyTaxesAndFees }}</div>
+        <hr /> <hr>
+        <div class="priceLabel grandTotal">Total</div>
+        <div class="price grandTotal" >
+          ${{
+            this.listing.price * this.daysSelected +
+            this.cleaningFee +
+            this.serviceFee +
+            this.occupancyTaxesAndFees
+          }}
+        </div>
+      </div>
     </div>
   </v-card>
 </template>
@@ -65,7 +59,7 @@ export default {
 
   props: {
     listing: Object,
-    reviews: Array
+    reviews: Array,
   },
   data() {
     return {
@@ -74,19 +68,23 @@ export default {
       cleaningFee: 50,
       serviceFee: 20,
       occupancyTaxesAndFees: 34,
-      averageReview:false
+      averageReview: false,
     };
   },
   methods: {
-      averageRating(){
-                for(let i = 0; i < this.reviews.length; i++){
-                    this.averageReview = this.averageReview + this.reviews[i].starValue;
-                }
-                return this.averageReview = (this.averageReview / this.reviews.length).toFixed(1) + ' (' + this.reviews.length + ' reviews)';
-            }            
+    averageRating() {
+      for (let i = 0; i < this.reviews.length; i++) {
+        this.averageReview = this.averageReview + this.reviews[i].starValue;
+      }
+      return (this.averageReview =
+        (this.averageReview / this.reviews.length).toFixed(1) +
+        " (" +
+        this.reviews.length +
+        " reviews)");
+    },
   },
   created() {
     this.averageRating();
-  }
+  },
 };
 </script>
